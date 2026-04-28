b856623ce0ab121cceaa5aa00dea4f34de71fdce
Lunarian-Nova <ckeister@anamosa.k12.ia.us>
2026-04-28 16:43:56 +0000

    ballz

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+// Global variable
+let distanceInMiles = 100;
+
+// Function
+function estimatedArrivalTime() {
+  // Local variable
+  let milesPerHour = 50;
+  // Return calculation
+  return distanceInMiles / milesPerHour;
+}
+
+// Set the result element
+document.getElementById("result").innerHTML = estimatedArrivalTime();
 
